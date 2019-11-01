2c49e23204d8a6ece70597c20217dace0b3e0968
davidszegedi <dszegedi@redhat.com>
2019-11-01 19:00:09 +0100

    broken code

--- src/hello/main.go
+++ src/hello/main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func main() {
-        <this time hopefully broken code>
+        <another round broken code>
 	router := NewRouter()
 
 	log.Fatal(http.ListenAndServe(":8000", router))
